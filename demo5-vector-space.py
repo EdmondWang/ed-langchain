75585e9565c3e523c8ca80730c6eb1a18fa1200d
edmond <644568491@qq.com>
2025-05-31 09:09:40 +0800

    clean API key in code

--- demo5-vector-space.py
+++ demo5-vector-space.py
@@ -10,9 +10,7 @@ from sentence_transformers import SentenceTransformer
 from langchain_huggingface import HuggingFaceEmbeddings
 
 os.environ['LANGSMITH_TRACING'] = 'true'
-os.environ['LANGSMITH_API_KEY'] = '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'
 os.environ['LANGSMITH_PROJECT']='ed-langchain'
-os.environ['DEEPSEEK_API_KEY']='sk-b99a159e8e3046ea9482d13d53e6e23a'
 
 llm = ChatDeepSeek(model ='deepseek-chat')
 
